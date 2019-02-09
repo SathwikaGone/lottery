964160cd046340401a07b3edf481cf7a5400cd0e
Sathwika <gonesathwika@gmail.com>
2019-02-08 18:52:39 -0600

    Commit changed button to image button, added labels,arranged in increasing order

---
+++ README.md
@@ -0,0 +1,38 @@
+Mobile	Computing	Android
+
+<p>Goal:</p>	
+Build	a	simple	app	with	a	grid	layout	that	helps	a	person	select	numbers	for	a	
+lottery.</br>
+Requirements:</br>
+1) You	must	use	a	grid	with	a	least	12 locations. There	must	be	at	least	two	
+rows.	There	must	be	at	least	two	columns.		The	widgets	must	include	buttons	
+and	TextEdits.</br>
+2) The TextEdits	allow	you	to	select	six	numbers	for	the	lottery.		Five	are	
+regular	and	can	be	any	value	from	1	to	69.		The	sixth	is	for	the powerball	and	
+can	be	from	1	to	26.	 The	TextEdits in	the	grid	must:</br>
+a. Allow	the	entry	of	a	number.	</br>	
+b. Use	white	background	for	the	5	regular	numbers	and	red	for	the	
+single	bonus	number.</br>
+3) Six	Buttons	will	allow	the	player	to	generate	a	random	value	and	place	it	in	
+the	corresponding	TextEdit</br>
+a. On	click	it	will	generate	a	random	value	and	put	it in	the	EditText.</br>
+b. Use	a	margin	or	padding	to	provide	some	separation	of	the	buttons.</br>
+4) There	should	be	a	two	buttons	and	a	text	view	below	the	grid.	</br>	
+a. The	clear	button	will	clear	all	the	values	in	the	EditTexts</br>
+b. The	Buy	button	will	put	the	message	"Purchased:" followed	by	all	
+number	choices.</br>
+c. The	TextView	will	record	a	ticket	purchase</br>
+<p>Bonus:</p>	
+Ø Guarantee	that	the	5	regular	numbers	are	different	before	you	purchase.		If	
+not,	set	a	message	"Duplicate	number."</br>
+Ø Guarantee	that	the	numbers	are	in	the	correct	range	before	you	purchase.		
+Set	a	message	if	not.</br>
+Ø When	you	print	the	5	regular	numbers	on	purchase,	put	them	in	increasing	
+order.</br>
+Ø Add	images	to	the	buttons.</br>
+Ø Add	buttons	that	will	increase	the	corresponding	value	by	1.</br>
+Ø Add	a	button	that	will	randomize	all	the	numbers.</br>
+Ø Add	descriptive	labels.</br>
+Ø Have	the	app	change	the	background	color	of	the	text	when	the	button	is	
+pressed.</br>
+Ø Use	a	TableLayout	instead	of	a	GridLayout</br>
--- app/src/main/java/com/example/lottery/MainActivity.java
+++ app/src/main/java/com/example/lottery/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.util.Arrays;
 
 import java.util.Random;
 
@@ -38,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         editText5.setText("");
         editText6.setText("");
         textView.setText("");
+        editText6.setBackgroundColor(Color.RED);
     }
     public void random(View v){
         EditText editText1 = findViewById(R.id.editText1);
@@ -49,37 +51,37 @@ public class MainActivity extends AppCompatActivity {
 
         switch(v.getId()){
             case R.id.button3 :
-                int r1 = new Random().nextInt(69);
+                int r1 = new Random().nextInt(69)+1;
                 String s1=String.valueOf(r1);
                 editText1.setText(s1);
                 editText1.setBackgroundColor(Color.YELLOW);
                 break;
             case R.id.button4 :
-                int r2 = new Random().nextInt(69);
+                int r2 = new Random().nextInt(69)+1;
                 String s2=String.valueOf(r2);
                 editText2.setText(s2);
                 editText2.setBackgroundColor(Color.YELLOW);
                 break;
             case R.id.button5 :
-                int r3 = new Random().nextInt(69);
+                int r3 = new Random().nextInt(69)+1;
                 String s3=String.valueOf(r3);
                 editText3.setText(s3);
                 editText3.setBackgroundColor(Color.YELLOW);
                 break;
             case R.id.button9 :
-                int r4 = new Random().nextInt(69);
+                int r4 = new Random().nextInt(69)+1;
                 String s4=String.valueOf(r4);
                 editText4.setText(s4);
                 editText4.setBackgroundColor(Color.YELLOW);
                 break;
             case R.id.button7 :
-                int r5 = new Random().nextInt(69);
+                int r5 = new Random().nextInt(69)+1;
                 String s5=String.valueOf(r5);
                 editText5.setText(s5);
                 editText5.setBackgroundColor(Color.YELLOW);
                 break;
             case R.id.button8 :
-                int r = new Random().nextInt(26);
+                int r = new Random().nextInt(26)+1;
                 String s=String.valueOf(r);
                 editText6.setText(s);
                 break;
@@ -94,12 +96,20 @@ public class MainActivity extends AppCompatActivity {
         EditText editText5 = findViewById(R.id.editText5);
         EditText editText6 = findViewById(R.id.editText6);
         TextView textView =findViewById(R.id.textView);
-        String e1 = editText1.getText().toString();
-        String e2 = editText2.getText().toString();
-        String e3 = editText3.getText().toString();
-        String e4 = editText4.getText().toString();
-        String e5 = editText5.getText().toString();
-       String e6 = editText6.getText().toString();
-        textView.setText("Purchased: "+e1+","+e2+","+e3+","+e4+","+e5+", Power:"+e6+".");
+         int e1 =Integer.valueOf( editText1.getText().toString());
+        int e2 = Integer.parseInt(editText2.getText().toString());
+        int e3 = Integer.parseInt(editText3.getText().toString());
+        int e4 = Integer.parseInt(editText4.getText().toString());
+        int e5 = Integer.parseInt(editText5.getText().toString());
+       int e6 = Integer.parseInt(editText6.getText().toString());
+       int arr[] = {e1,e2,e3,e4,e5};
+        //int[] a1 = new int[5];
+        Arrays.sort(arr);
+        if((e1<=0 || e1>69) || (e2<=0 || e2>69) || (e3<=0 || e3>69)|| (e4<=0 || e4>69) ||( e5<=0 || e5>69) || (e6<=0 || e6>26)){
+            textView.setText("The selected Number is out of range \n 1) power button 1-26 \n 2) Other buttons 1-69");
+  }
+  else{
+            textView.setText("Purchased: "+arr[0]+","+arr[1]+","+arr[2]+","+arr[3]+","+arr[4]+", Power:"+e6+".");
+        }
     }
 }
